48e5654474466dd8f038a372a8d54790a5ac9732
Rahul Doshi <rdoshi@netflix.com>
2016-04-06 16:23:06 -0700

    fix refresh script

--- scripts/refresh.sh
+++ scripts/refresh.sh
@@ -12,14 +12,14 @@ SOURCE_DIR=${BASE_DIR}/${DOWNLOAD_URI}
 # download all map files
 wget -c -N -r -np -nv -A js,svg,geo.json ${DOWNLOAD_URL}/${DOWNLOAD_URI}
 
-# keep only relevant files and/or folders
-mv ${BASE_DIR}/${DOWNLOAD_URL}/${DOWNLOAD_URI} ${SOURCE_DIR}
-rm -rf ${BASE_DIR}/${DOWNLOAD_URL}
-
 # marshal files into their respective folders
-cd ${SOURCE_DIR}/custom
+cd ${BASE_DIR}/${DOWNLOAD_URL}/${DOWNLOAD_URI}/custom
 for map_file in *{svg,js,geo.json}; do
     folder="${map_file%%.*}"
     mkdir -p "$folder"
     mv "$map_file" "$folder"
 done
+
+# keep only relevant files and/or folders
+rsync -a ${BASE_DIR}/${DOWNLOAD_URL}/${DOWNLOAD_URI}/ ${SOURCE_DIR}
+rm -rf ${BASE_DIR}/${DOWNLOAD_URL}
