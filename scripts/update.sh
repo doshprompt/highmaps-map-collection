f23f7f1995e43bb347c5939005b88f5beae822e0
Rahul Doshi <rdoshi@netflix.com>
2016-04-06 17:02:01 -0700

    fix update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,14 +2,6 @@
 
 set -e # exit with nonzero exit code if anything fails
 
-# clear and re-create the out directory
-rm -rf out || exit 0;
-mkdir out;
-
-# go to the out directory and create a *new* Git repo
-cd out
-git init
-
 # inside this git repo we'll pretend to be a new user
 git config user.name "Travis CI"
 git config user.email "rahuld223@yahoo.com"
